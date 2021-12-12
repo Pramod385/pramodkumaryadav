0ac2014c4a205a47cf228a4ca2298824b77353bc
Pramod385 <95876802+Pramod385@users.noreply.github.com>
2021-12-12 14:50:35 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,33 @@
+a='Shalendra'
+print(a)
+
+a,b=10,20
+a,b=b,a
+print(a)
+print(b)
+
+a,b=10,20
+a,b=b,a
+print(a,end=' ')
+print(b)
+
+print("8","12","2021",sep="-")
+
+user_name=input("enter user name")
+print(user_name)
+
+a=input("first no")
+b=input("second no")
+print(a+b)
+
+name="pra"
+age="20"
+print("I am",name,"My age is",age)
+print("I am {} My age is{}".format(name,age))
+
+print("I am {1} My age is{0}".format(name,age))
+
+
+
+
+
